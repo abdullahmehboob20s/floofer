f429743057cd3d6c992c34bbea0d64d735ae56d3
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-02-15 19:02:47 +0500

    navbar done

---
+++ src/components/UI/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { FaTwitter } from "react-icons/fa";
+
+function Header() {
+  return (
+    <nav className="bg-header h-header-height">
+      <div className="container-wrapper flex h-full items-center justify-between">
+        <div className="flex items-center space-x-4">
+          <img
+            src="images/cropped-logo.png"
+            className="w-[90px] logo-shadow relative top-3"
+            alt=""
+          />
+          <p className="text-3xl font-huglove text-white">FLooFER</p>
+        </div>
+
+        <div className="flex items-center space-x-7 text-white uppercase">
+          <p className="font-marsmelloe text-xl">Home</p>
+          <p className="font-marsmelloe text-xl">Roadmap</p>
+          <p className="font-marsmelloe text-xl">Whitepaper</p>
+          <p className="font-marsmelloe text-xl">Community</p>
+          <p className="font-marsmelloe text-xl">Telegram</p>
+
+          <div className="divider-vertical h-[28px] border-purple-1"></div>
+
+          <a
+            target="_blank"
+            href="https://twitter.com"
+            className="flex items-center space-x-3"
+          >
+            <FaTwitter className="text-twitter text-2xl" />{" "}
+            <span className="font-marsmelloe text-xl">Twitter</span>
+          </a>
+
+          <p className="font-marsmelloe text-xl">buy now</p>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      poppins: "'Poppins', sans-serif",
+      huglove: "'huglove', sans-serif",
+      marsmelloe: "'marsmelloe', sans-serif",
+    },
+    colors: {
+      white: "white",
+      black: "black",
+      transparent: "transparent",
+      primary: "#FFDD84",
+      "primary-2": "#FFE074",
+      brown: "#994B1F",
+
+      blue: {
+        link: "#05CAEB",
+        text: "#355560",
+      },
+
+      purple: {
+        1: "#A3A4CA",
+      },
+
+      // SOCIALS
+      twitter: "#03A9F4",
+    },
+    extend: {
+      backgroundImage: {
+        header: "linear-gradient(180deg, #000000 0%, #4F4F4F 100%)",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      spacing: {
+        "header-height": "var(--header-height)",
+      },
+    },
+  },
+  plugins: [],
+};
