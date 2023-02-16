20df328012fccbed2ce7a34f02a63d075d17940b
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-02-16 15:05:13 +0500

    made text more smaller

--- src/components/UI/HowToBuyCard.js
+++ src/components/UI/HowToBuyCard.js
@@ -3,7 +3,7 @@ import React from "react";
 function HowToBuyCard({ children, desc }) {
   return (
     <div className="border-[4px] xl:border-[6px] border-black py-6 xl:py-12 px-8 xl:px-10 rounded-2xl xl:rounded-[36px] bg-[rgba(255,255,255,0.71)] shadow-[4px_4px_8px_rgba(0,0,0,0.25)]">
-      <p className="font-semibold text-sm xl:text-lg textblack tracking-[0.04em]">
+      <p className="font-semibold text-xs sm:text-sm xl:text-lg textblack tracking-[0.04em]">
         {desc}
       </p>
     </div>
