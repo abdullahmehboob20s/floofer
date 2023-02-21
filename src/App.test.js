af1bb24b02ab5299f6458de4c799f3f44235c638
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-02-21 11:34:39 +0500

    wrote test to test frontend on different browsers

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { test } from "@playwright/test";
+
+test("test browser", async ({ page }) => {
+  // point this to wherever you want
+  await page.goto("http://localhost:3000/");
+
+  // keep browser open
+  await page.pause();
 });
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
